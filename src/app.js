b29f7ce3fca5c7c4f752d893e8f5160bce9df311
amandollar <toxicempire3@gmail.com>
2026-01-09 00:35:12 +0530

    feat: add body-parser and morgan-body middleware, update user.controller import path

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
+import morganBody from "morgan-body";
+import bodyParser  from "body-parser";
 
 const app = express();
 
@@ -12,10 +14,14 @@ app.use(
   })
 );
 
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 app.use(cookieParser());
+morganBody(app);
+
 
 app.get("/", (_req, res) => {
   res.status(200).json({
@@ -35,5 +41,7 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server error",
   });
 });
+
+
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import generateAccessAndRefreshTokens from "../utils/GenerateToken..js";
+import generateAccessAndRefreshTokens from "../utils/GenerateToken.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
